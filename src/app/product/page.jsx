a47cf698b6301745a058997a01405e2eee380a48
Anoteros09 <insanity99coc@gmail.com>
2025-01-12 20:00:15 +0530

    filter changes for price, rating & discount

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -1,24 +1,28 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useProductStore from "./store";
 import ProductCard from "../component/ProductCard";
+import { FilterProducts } from "../utils/FilterProducts";
 
 function page() {
   const {
-    priceRange,
-    rating,
-    brands,
-    categories,
+    selPriceRange,
     discount,
+    selBrands,
+    selCategories,
+    rating,
     setProducts,
     products,
     filter,
+    setFilter,
   } = useProductStore((state) => state);
+  const [filteredList, setFilteredList] = useState([]);
   const fetchProducts = async () => {
-    const { products } = await fetch(
+    const { products: productsData } = await fetch(
       "https://dummyjson.com/products?limit=0"
     ).then((res) => res.json());
-    setProducts(products);
+    setProducts(productsData);
+    setFilteredList(productsData);
   };
   useEffect(() => {
     fetchProducts();
@@ -26,11 +30,21 @@ function page() {
 
   useEffect(() => {
     if (filter) {
+      setFilteredList(
+        FilterProducts(products, {
+          selPriceRange,
+          discount,
+          selBrands,
+          selCategories,
+          rating,
+        })
+      );
+      setFilter(false);
     }
   }, [filter]);
   return (
     <div className="grid grid-cols-1 gap-4 p-6">
-      {products.map((product) => {
+      {filteredList.map((product) => {
         return <ProductCard product={product} key={product.id} />;
       })}
     </div>
